8b169a5d8af7cd5a97801a5fd308b849d1dd5f94
fukiran <macdorphoto@yahoo.com>
2021-02-09 19:07:01 +0000

    library has collection, can add book, tested

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
 
+    private ArrayList<Book> collectionOfBooks;
+
+    public Library() {
+        this.collectionOfBooks = new ArrayList<Book>();
+    }
+
+    public int getNumberOfBooks() {
+        return collectionOfBooks.size();
+    }
+
+    public void addBook(Book book) {
+        collectionOfBooks.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
 
+    Library library;
+    Book book;
+
+    @Before
+    public void setUp(){
+        book = new Book("Core Java", "Horstmann", "Computer Book");
+        library = new Library();
+
+    }
+
+
+
+    @Test
+    public void hasCollectionOfBooks(){
+        assertEquals(0, this.library.getNumberOfBooks());
+    }
+
+
+    @Test
+    public void canAddBook(){
+        library.addBook(book);
+        assertEquals(1, this.library.getNumberOfBooks());
+    }
 }
